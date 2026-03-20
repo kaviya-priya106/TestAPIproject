0741acf14fccf157a7564cd0348fc25bc003a8d4
Kaviya Sivasankaran <kaviya.sivasankaran@claysys.net>
2026-03-20 22:40:13 +0530

    register authservice

--- TestAPIproject/Program.cs
+++ TestAPIproject/Program.cs
@@ -78,7 +78,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // 🔹 Dependency Injection
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 // 🔹 AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
